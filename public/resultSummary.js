910013fa3ef1dbf04fd393d552964277d61a2214
ferencprem2 <fercsusz0508@gmail.com>
2024-01-03 16:47:43 +0100

    ResultSummary done

--- public/resultSummary.js
+++ public/resultSummary.js
@@ -7,17 +7,18 @@ const resultDiv = document.getElementById('results')
 const doneButton = document.getElementById('sendButton')
 const editButton = document.getElementById("editButton")
 
-export function showSummary(DataArray, questionArray) {
+export function showSummary(DataObject, questionArray) {
     if (!resultSummary) {
         console.error('The "offer-summary" element was not found in the DOM. Make sure it exists.');
         return;
     }
 
     let summaryText = '';
-    for (let i = 0; i < DataArray.length; i++) {
-        console.log(questionArray[i])
-        summaryText += `<div class="summary-container"> <p>${i + 1}. adat: ${questionArray[i]}:</p> <input type="text" value="${DataArray[i]}" readonly></input></div>`
-    }
+    Object.keys(DataObject).forEach((key, index) => {
+        console.log(questionArray[index]);
+        summaryText += `<div class="summary-container"> <p>${index + 1}. adat: ${questionArray[index]}:</p> <input type="text" value="${DataObject[key]}" readonly></input></div>`;
+    });
+    
     resultDiv.style.visibility = 'visible';
     resultSummary.innerHTML = summaryText;
 
@@ -26,7 +27,7 @@ export function showSummary(DataArray, questionArray) {
     let isEditable = false;
     var newInputValues = []
     editButton.addEventListener("click", () => {
-        console.log(DataArray)
+        console.log(DataObject)
         // editButton.style.backgroundColor = 'darkgreen';
         isEditable ? editButton.style.backgroundColor = '#a60303' : editButton.style.backgroundColor = 'darkgreen';
         if (isEditable) {
@@ -42,7 +43,7 @@ export function showSummary(DataArray, questionArray) {
         //TODO: Confirmation for the datas, yes or no-
         isOptionSelected = false;
         const URL = "http://localhost:8080/"
-        switch(DataArray) {
+        switch(DataObject) {
             case measurementDatasArray: 
                 console.log("freeAsd")
                 sendJsonData(measurementDatasArray, URL, "setMeasurementData")
